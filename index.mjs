35f5c8d49004cab32d7eb43ae8b3972bfef5ed6f
Stian Toften <stiantoften@gmail.com>
2020-11-10 11:45:33 +0100

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,15 @@
+import fetch from "node-fetch";
+import fs from "fs/promises";
+
+const wl = await fs.readFile("wl.txt");
+
+String(wl)
+  .split("\n")
+  .map(async (link) => {
+    const res = await fetch(`http://agder-ikt70.uia.no:17003/${link}`);
+    const text = await res.text();
+
+    if (text !== "Nope not here!! Keep looking :D") {
+      console.log(`check out: ${link}`);
+    }
+  });

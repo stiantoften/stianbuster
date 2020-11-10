a958fbb54d6e831ec2c6ed0cc190ab02f9d0ca67
Stian Toften <stiantoften@gmail.com>
2020-11-10 11:57:54 +0100

    fix

--- index.mjs
+++ index.mjs
@@ -7,11 +7,11 @@ const list = String(wl).split("\n");
 
 for (let i = 0; i < list.length; i++) {
   try {
-    const res = await fetch(`http://agder-ikt70.uia.no:17003/${link}`);
+    const res = await fetch(`http://agder-ikt70.uia.no:17003/${wl[i]}`);
     const text = await res.text();
 
     if (text !== "Nope not here!! Keep looking :D") {
-      console.log(`check out: ${link}`);
+      console.log(`check out: ${wl[i]}`);
     }
   } catch (e) {
     console.error(e);

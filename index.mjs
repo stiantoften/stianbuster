b90a015205f44cd09def1d701591f1d71e6d091a
Stian Toften <stiantoften@gmail.com>
2020-11-10 11:56:58 +0100

    remove async

--- index.mjs
+++ index.mjs
@@ -3,17 +3,17 @@ import fs from "fs/promises";
 
 const wl = await fs.readFile("wl.txt");
 
-String(wl)
-  .split("\n")
-  .map(async (link) => {
-    try {
-      const res = await fetch(`http://agder-ikt70.uia.no:17003/${link}`);
-      const text = await res.text();
+const list = String(wl).split("\n");
 
-      if (text !== "Nope not here!! Keep looking :D") {
-        console.log(`check out: ${link}`);
-      }
-    } catch (e) {
-      console.error(e);
+for (let i = 0; i < list.length; i++) {
+  try {
+    const res = await fetch(`http://agder-ikt70.uia.no:17003/${link}`);
+    const text = await res.text();
+
+    if (text !== "Nope not here!! Keep looking :D") {
+      console.log(`check out: ${link}`);
     }
-  });
+  } catch (e) {
+    console.error(e);
+  }
+}
